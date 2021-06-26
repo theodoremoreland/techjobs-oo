80488207216abff4bcd03034fc546c3e10549dfb
Theodore Moreland <theodoremoreland@yahoo.com>
2021-06-26 17:54:51 -0500

    App now creates job via user input. Jobs now print to console with custom format (i.e. colors, decorations, etc).

--- src/org/launchcode/techjobs_oo/Main.java
+++ src/org/launchcode/techjobs_oo/Main.java
@@ -1,21 +1,56 @@
 package org.launchcode.techjobs_oo;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+
+import static org.launchcode.techjobs_oo.CustomFormatter.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Job job1 = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
-        Job job2 = new Job("Web Developer", new Employer("LaunchCode"), new Location("St. Louis"), new PositionType("Front-end developer"), new CoreCompetency("JavaScript"));
-        Job job3 = new Job("Ice cream taster", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Taste"));
         ArrayList<Job> jobs = new ArrayList<>();
+        Scanner in = new Scanner(System.in);
 
-        jobs.add(job1);
-        jobs.add(job2);
-        jobs.add(job3);
+        System.out.print("\n\nHow many job postings do you want to create? ");
+        int numberOfJobPostingsToCreate = in.nextInt();
+        in.nextLine(); // Consumes the "enter" button pressed after int input.
+        System.out.println("\nCreating " + colorWrap(String.valueOf(numberOfJobPostingsToCreate), BLUE) + " job postings...");
+        System.out.println(colorWrap("Enter the values for each field of the job posting as they are prompted below, then press your 'ENTER' key.\n", GREEN_BOLD_BRIGHT));
+
+        while (numberOfJobPostingsToCreate > 0) {
+            String name;
+            String employer;
+            String location;
+            String positionType;
+            String coreCompetency;
+            Job job;
+
+            System.out.print(colorWrap("CREATE NEW JOB POSTING\n", BLUE_UNDERLINED));
+            System.out.print("    Name: ");
+            name = in.nextLine();
+
+            System.out.print("    Employer: ");
+            employer = in.nextLine();
+
+            System.out.print("    Location: ");
+            location = in.nextLine();
+
+            System.out.print("    Position Type: ");
+            positionType = in.nextLine();
+
+            System.out.print("    Core competency: ");
+            coreCompetency = in.nextLine();
+
+            job = new Job(name, new Employer(employer), new Location(location), new PositionType(positionType), new CoreCompetency(coreCompetency));
+            jobs.add(job);
+
+            numberOfJobPostingsToCreate--;
+        }
+
+        System.out.println(colorWrap("\n*****JOBS CREATED*****", WHITE_BOLD_BRIGHT));
 
         for (Job job : jobs){
-            System.out.println(job);
+            System.out.println(colorWrap(job.toString(), BLUE_BRIGHT));
         }
     }
 
